f3238f0e44ae954467ab80f8a7c3046d7d30b466
Gnorium <info@gnorium.com>
2025-12-25 17:46:42 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,33 +12,72 @@ let package = Package(
         .visionOS(.v1)
     ],
     products: [
-        .library(
-            name: "WebFormats",
-            targets: ["WebFormats"]
-        ),
+        .library(name: "JSONFormat", targets: ["JSONFormat"]),
+        .library(name: "JSONLDFormat", targets: ["JSONLDFormat"]),
+        .library(name: "JSONImportMapFormat", targets: ["JSONImportMapFormat"]),
+        .library(name: "WAMFormat", targets: ["WAMFormat"]),
     ],
     dependencies: [
         .package(url: "https://github.com/gnorium/web-types", from: "1.0.0")
     ],
     targets: [
         .target(
-            name: "WebFormats",
-            dependencies: [
-                .product(name: "WebTypes", package: "web-types")
-            ],
+            name: "JSONFormat",
+            dependencies: [],
+            path: "Sources/WebFormats/JSONFormat",
             swiftSettings: [
                 .enableUpcomingFeature("BareSlashRegexLiterals"),
                 .enableUpcomingFeature("ConciseMagicFile"),
                 .enableUpcomingFeature("ExistentialAny"),
                 .enableUpcomingFeature("ForwardTrailingClosures"),
                 .enableUpcomingFeature("ImplicitOpenExistentials"),
-                .enableUpcomingFeature("StrictConcurrency"),
-                .unsafeFlags(["-warn-concurrency"], .when(configuration: .debug)),
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+        .target(
+            name: "JSONLDFormat",
+            dependencies: ["JSONFormat"],
+            path: "Sources/WebFormats/JSONLDFormat",
+            swiftSettings: [
+                .enableUpcomingFeature("BareSlashRegexLiterals"),
+                .enableUpcomingFeature("ConciseMagicFile"),
+                .enableUpcomingFeature("ExistentialAny"),
+                .enableUpcomingFeature("ForwardTrailingClosures"),
+                .enableUpcomingFeature("ImplicitOpenExistentials"),
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+        .target(
+            name: "JSONImportMapFormat",
+            dependencies: ["JSONFormat"],
+            path: "Sources/WebFormats/JSONImportMapFormat",
+            swiftSettings: [
+                .enableUpcomingFeature("BareSlashRegexLiterals"),
+                .enableUpcomingFeature("ConciseMagicFile"),
+                .enableUpcomingFeature("ExistentialAny"),
+                .enableUpcomingFeature("ForwardTrailingClosures"),
+                .enableUpcomingFeature("ImplicitOpenExistentials"),
+                .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+        .target(
+            name: "WAMFormat",
+            dependencies: [
+                .product(name: "WebTypes", package: "web-types")
+            ],
+            path: "Sources/WebFormats/WAMFormat",
+            swiftSettings: [
+                .enableUpcomingFeature("BareSlashRegexLiterals"),
+                .enableUpcomingFeature("ConciseMagicFile"),
+                .enableUpcomingFeature("ExistentialAny"),
+                .enableUpcomingFeature("ForwardTrailingClosures"),
+                .enableUpcomingFeature("ImplicitOpenExistentials"),
+                .enableUpcomingFeature("StrictConcurrency")
             ]
         ),
         .testTarget(
             name: "WebFormatsTests",
-            dependencies: ["WebFormats"]
+            dependencies: ["JSONFormat", "JSONLDFormat", "JSONImportMapFormat", "WAMFormat"]
         ),
     ]
 )
